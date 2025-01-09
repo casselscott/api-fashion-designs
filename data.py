2feb76d6f949cb84bdcbd39720f3c8486737eb1f
cassel scott-emuakpor <casselscott-emuakpor@cassels-MacBook-Air.local>
2025-01-09 04:31:18 +0100

    file updated

---
+++ data.py
@@ -0,0 +1,269 @@
+# Dummy data
+products = [
+  
+     {
+        "id": 1,
+        "name": "Diva Bridal Gowns",
+        "description": "A luxurious beaded silk dress perfect for weddings.",
+        "brand": "Hanel",
+        "image": ["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736344077/diva_bridal_gowns_yxkoar.jpg"],
+        "categories": ["Evening Wear", "Dresses", "Weddings"]
+  },
+  {
+        "id": 2,
+        "name": "Viva Party Collection",
+        "description": "A stylish and comfortable party dresses for every occasion.",
+        "brand": "Evi's",
+        "image": ["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736344022/viva_party_collection_bspyfe.jpg"],
+        "categories": ["Evening Wear", "Dresses", "Weddings"]
+  },
+  {
+        "id": 3,
+        "name": "Wedding Party Collection",
+        "description": "A beautiful Collection of Wedding Party Dresses.",
+        "brand": "Ara",
+        "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736343869/wedding_party_collection_ijecs9.jpg"],
+        "categories": ["Dresses", "Casual Wear", "Evening Wear", "Weddings"]
+  },
+  {
+       "id": 4,
+       "name": "Kimora Elegant Gowns",
+       "description": "Elegant gowns designed for comfort and all occasions.",
+       "brand": "Nira",
+       "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736343706/kimora_elegant_gown_uykeaw.jpg"],
+       "categories": ["Dresses", "Casual Wear", "Evening Wear", "Weddings"]
+  },
+
+  {
+      "id": 5,
+      "name": "Turtle Neck Beaded Gown",
+      "description": "A stylish turtle neck beaded gown for all occasions.",
+      "brand": "Nira",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736343630/stylish_turtle-neck_beaded_gown_tczggw.jpg"],
+      "categories": ["Dresses", "Casual Wear", "Evening Wear", "Weddings"]
+  },
+  {
+      "id": 6,
+      "name": "Lala African Party Gowns",
+      "description": "A luxurious silk dress perfect for evening wear.",
+      "brand": "Hanel",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736343511/lala_african_party_gowns_nk1tws.jpg"],
+      "categories": ["Evening Wear", "Dresses"]
+  },
+  {
+      "id": 7,
+      "name": "Sora Elite Gown",
+      "description": "A classic, stylish and party gown for all Occasions.",
+      "brand": "Evi's",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736343374/sora_elite_gown_xcygkt.jpg"],
+      "categories": ["Casual", "Dresses", "Evening Wear", "Weddings"]
+  },
+  {
+      "id": 8,
+      "name": "The Beauty Pageant Gown",
+      "description": "A beautiful beaded dress with a flowy silhouette.",
+      "brand": "Ara",
+      "image": ["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736343307/beauty_pagent_gowns_qv1ekp.jpg"],
+      "categories": ["Dresses", "Evening Wear", "Weddings"]
+  },
+  {
+      "id": 9,
+      "name": "Zoe Bridal Gowns",
+      "description": "Bridal gowns designed for exquisite Brides.",
+      "brand": "Nira",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736343202/zoe_bridal_gowns_mauxjn.jpg"],
+      "categories": ["Casual", "Dresses", "Evening Wear", "Weddings"]
+  },
+  {
+      "id": 10,
+      "name": "Stunning Pink Party Gown",
+      "description": "A stylish beaded gown for stunning ladies.",
+      "brand": "Hanel",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736343060/elegant_pink_bridal_gown_z02ndu.jpg"],
+      "categories": ["Dresses", "Evening Wear", "Weddings"]
+  },
+  {
+      "id": 11,
+      "name": "Ella Stunning Bridal Gowns",
+      "description": "Vibrant Stunning Party Gowns.",
+      "brand": "M&C",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736342963/Ella_stunning_bridal_gowns_ufrdzr.jpg"],
+      "categories": ["Dresses", "Evening Wear", "Weddings"]
+  },
+  {
+      "id": 12,
+      "name": "Cleo Gowns",
+      "description": "Party Dresses for the everyday woman.",
+      "brand": "Nira",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736342917/cleo_gowns_fwsk0z.jpg"],
+      "categories": ["Dresses", "Evening Wear", "Weddings"]
+  },
+  {
+      "id": 13,
+      "name": "Funke Beaded Party Gowns",
+      "description":"A simple, comfortable african beautiful gowns  made of high-quality.",
+      "brand": "Evi's",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736342864/funke_beaded_party_gown_lmoe2c.jpg"],
+      "categories": ["Dresses", "Casual"]
+  },
+  {
+      "id": 14,
+      "name": "Coral Gowns",
+      "description": "Beautiful gown designed to create a sleek silhouette.",
+      "brand": "Hanel",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736342817/coral_gown_l3tswz.jpg"],
+      "categories": ["Dresses", "Casual"],
+  },
+  {
+      "id": 15,
+      "name": "Bella Party Dresses",
+      "description": "A soft and gorgeous dress, ideal for all party occasions.",
+      "brand": "Divalane",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736342685/bella_party_dresses_py32kl.jpg"],
+      "categories": ["Dresses", "Weddings", "Evening Wear"]
+  },
+  {
+      "id": 16,
+      "name": "The Kente African Jumpsuit",
+      "description": "African Jumpsuit with delicate embroidery.",
+      "brand": "Divalane",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736342615/kente_african_jumpsuit_mb7yay.jpg"],
+      "categories": ["Casual", "Evening Wear"]
+  },
+  {
+      "id": 17,
+      "name": "The African Wedding Dress",
+      "description": "A tailored african print for a sharp and sophisticated look.",
+      "brand": "Divalane",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736342553/african_wedding_party_dresses_cwftoc.jpg"],
+      "categories": ["Wedding", "Casual"]
+  },
+  {
+      "id": 18,
+      "name": "Monalisa Party Gown",
+      "description": "A fully beaded gown with a classic plaid design.",
+      "brand": "Divalane",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736342494/monalisa_beaded_party_gown_mxxcjv.jpg"],
+      "categories": ["Weddings", "Evening Wear", "Dresses"]
+  },
+  {
+      "id": 19,
+      "name": "Meg Party Gown",
+      "description": "Comfortable gown with a slim fit and great stretch.",
+      "brand": "Divalane",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736342438/meg_beaded_party_gown_aspvem.jpg"],
+      "categories": ["Evening Wear", "Dresses", "Casual"]
+  },
+  {
+      "id": 20,
+      "name": "Sara Beaded Party Gown",
+      "description": "A delicate silk beaded gown with an elegant design.",
+      "brand": "Hanel",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736342368/sara_beaded_party_gown_fjf9gf.jpg"],
+      "categories": ["Evening Wear", "Dresses", "Wedding"]
+  },
+  {
+      "id": 21,
+      "name": "Ladies Party Gowns",
+      "description": "A beautiful slim fit dress for all occassions.",
+      "brand": "Evi's",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736342319/ladies_beaded_party_gowns_oqze4y.jpg"],
+      "categories": ["Evening Wears", "Casual", "Dresses"]
+  },
+  {
+      "id": 22,
+      "name": "The V-neck Women African Jumpsuit",
+      "description":"Comfortable Casual Jumpsuit pants with a fit for an active lifestyle.",
+      "brand": "Ada's",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736342257/v-neck_women_african_jumpsuit_sy3br8.jpg"],
+      "categories": ["Evening Wears", "Casual"]
+  },
+  {
+      "id": 23,
+      "name": "The lady Playsuit",
+      "description":"A playful suit that fits like a glove, perfect for a night out.",
+      "brand": "Ada's",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736342188/Ladies_African_Tubeless_Jumpsuit_ze5mw5.jpg"],
+      "categories": ["Casual", "Evening Wear"]
+  },
+  {
+      "id": 24,
+      "name": "The Bubu Gown",
+      "description": "A light free flow gown that will keep you cozy all day.",
+      "brand": "Ada's",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736342098/blue_casual_african_bubu_gown_swfdrb.jpg"],
+      "categories": ["Casual", "Evening Wear"]
+  },
+  {
+      "id": 25,
+      "name": "Long Sleeve Jumpsuit",
+      "description": "Comfortable Jumpsuits that are perfect for daily use.",
+      "brand": "Evi's",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736342047/long_sleeve_women_jumpsuit_rxm9pa.jpg"],
+      "categories": ["Evening Wear", "Casual"]
+  },
+  {
+      "id": 26,
+      "name": "Blue Elegant Short Dress",
+      "description": "A boho chic elegant dresss.",
+      "brand": "Divalane",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736341968/beaded_blue_elegant_short_dress_aukfcb.jpg"],
+      "categories": ["Dresses", "Weddings", "Evening Wear"]
+  },
+  {
+      "id": 27,
+      "name": "Beaded Navy Blue Jumpsuit",
+      "description": "A flattering jumpsuit for all occasions.",
+      "brand": "Divalane",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736341784/beaded_navy_blue_jumpsuit_fi4wrg.jpg"],
+      "categories": ["Weddings", "Casual", "Evening Wear"]
+  },
+  {
+      "id": 28,
+      "name": "Classic African Bubu",
+      "description": "A timeless casual gown from soft, breathable cotton.",
+      "brand": "Ada's",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736341735/casual_african_bubu_gown_xs3xhs.jpg"],
+      "categories": ["Evening Wear", "Casual"],
+  },
+  {
+      "id": 29,
+      "name": "Diva Wedding Gown",
+      "description": "Stylish, beautiful Off shoulder Wedding Dresses.",
+      "brand": "Divalane",
+      "image":["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736341697/offshoulder_wedding_dress_nswrf8.jpg"],
+      "categories": ["Wedding", "Dresses"]
+  },
+  {
+      "id": 30,
+      "name": "The Short Gown African Print",
+      "description": "A soft african print short gown.",
+      "brand": "Ada's",
+      "image": ["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736341553/african_print_short_gown_ycsus6.jpg"],
+      "categories": ["Casual", "Dresses"]
+  },
+  {
+      "id": 31,
+      "name": "Pila Jumpsuit",
+      "description": "Stylish jumpsuit pants with a slim fit and modern cut.",
+      "brand": "Ada's",
+      "image": ["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736341483/african_print_jumpsuit_flenkm.jpg"],
+      "categories": ["Casual"]
+  },
+  {
+      "id": 32,
+      "name": "Ella Pink Gown",
+      "description": "Chic and comfortable beaded gowns with a strappy design.",
+      "brand": "Divalane",
+      "image": ["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736341379/pink_beaded_dress_with_a_sweetheart_qqxnks.jpg"],
+      "categories": ["Dresses", "Casual", "Evening Dress", "Wedding"]
+  },
+  {
+      "id": 33,
+      "name": "Multi Color Skirt",
+      "description": "A cool and multi color skirt for a casual, sophisticated look.",
+      "brand": "Evi's",
+      "image": ["https://res.cloudinary.com/dxvfabxw8/image/upload/v1736378364/women_african_multicolor_skirt_fn6fck.jpg"],
+      "categories": ["Casual", "Dresses"]
+  },
+]
